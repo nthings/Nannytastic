ade6f8142c451bd05eed88ebea06d99fa3fe396c
Mauricio Martinez <n_othing@hotmail.com>
2018-04-02 14:55:14 -0500

    Nanny Info Window

--- src/app/nanny-info-window/nanny-info-window.component.ts
+++ src/app/nanny-info-window/nanny-info-window.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/toArray';
 
 @Component({
   selector: 'app-nanny-info-window',
@@ -9,9 +12,12 @@ export class NannyInfoWindowComponent implements OnInit, OnDestroy {
   @Input()
   nanny;
 
+  stars;
+
   constructor() { }
 
   ngOnInit() {
+    this.stars = Observable.range(0, this.nanny.stars).toArray();
   }
 
   ngOnDestroy() {
